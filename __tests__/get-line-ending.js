19f9aba3fa1c8b0df2c6605f8ea8c04ef4ca6eb8
Saurav Sahu <mrsauravsahu@outlook.com>
2021-05-02 17:53:39 +0530

    Feat: file based tests (#3)
    * feat: setup file based  tests
    
    * add getLineEnding helper
    
    * fix: allow for CRLF in .env files
    
    * move file-samples to __tests__

---
+++ __tests__/get-line-ending.js
@@ -0,0 +1,13 @@
+const getLineEnding = require('../src/get-line-ending')
+
+describe(getLineEnding.name, () => {
+    test('should return LF if string contains only \\n', () => {
+        const inputString = 'NODE_ENV=development\nSECOND_ENV=second'
+        expect(getLineEnding(inputString)).toEqual('\n')
+    })
+
+    test('should return CRLF if string contains \\r\\n', () => {
+        const inputString = 'NODE_ENV=development\r\nSECOND_ENV=second'
+        expect(getLineEnding(inputString)).toEqual('\r\n')
+    })
+})
