c0c97e0fec53ed474792e0ae0467a97b4a0c2a50
Saurav Sahu <mrsauravsahu@outlook.com>
2021-05-02 17:08:21 +0530

    Feat: Add CLI project (#1)
    * initial commit
    
    * create node project
    
    * ignore node_modules folder
    
    * install jest for testing
    
    * ignore .env files
    
    * setup project structure
    
    * setup cli command
    
    * install typings for jest
    
    * strip env values from .env file
    
    * template for multi line .env files
    
    * preserve empty lines
    
    * handle comments in .env file
    
    * ignore template.env
    
    * read .env and write template .env file through cli
    
    * bump version to v1.0.0

---
+++ src/bin/cli.js
@@ -0,0 +1,17 @@
+#! env node
+
+const { promises } = require('fs')
+
+const genEnvTemplate = require("../gen-env-template");
+
+const envFilePath = process.argv[2]
+const outputPath = process.argv[3] || 'template.env'
+
+const cliGenEnvTemplateAsync = async() => {
+    const envFileString = await promises.readFile(envFilePath, { encoding: 'utf-8' })
+
+    const templateFileString = genEnvTemplate(envFileString)
+    await promises.writeFile(outputPath, templateFileString, { encoding: 'utf-8' })
+}
+
+cliGenEnvTemplateAsync()
---
+++ src/gen-env-template.js
@@ -0,0 +1,24 @@
+function genEnvTemplate(envFileString = '') {
+    if (envFileString === '') return ''
+
+    const templateFileString = envFileString
+        .split('\n')
+        .map(currentLineString => {
+            // If empty line
+            const isEmpty = currentLineString.trim() === ''
+            if (isEmpty) return currentLineString
+
+            // If comment
+            const isComment = currentLineString.trim()[0] === '#'
+            if (isComment) return currentLineString
+
+            var keyName = currentLineString.split('=')[0]
+            var templateForCurrentLine = `${keyName}=`
+            return templateForCurrentLine
+        })
+        .join('\n')
+
+    return templateFileString
+}
+
+module.exports = genEnvTemplate;
