44ad6e4410d330020b27554f50f5a3cebf38342e
Saurav Sahu <mrsauravsahu@outlook.com>
2021-05-05 22:23:38 +0530

    Improv: Generate one test case per file for shell and npx tests (#22)
    * generate separate test case for each file in shell tests
    
    * generate one test case per file for npx test case
    
    * update test group name for shell tests
    
    * remove testTimeout switch for jest
    
    * run tests in verbose mode in CI

--- __tests__/npx-tests.js
+++ __tests__/npx-tests.js
@@ -1,11 +1,11 @@
-const { promises } = require('fs')
+const { readFileSync, readdirSync } = require('fs')
 const shell = require('shelljs')
 const { name: packageName } = require('../package.json')
 
 const basePath = `${__dirname}/file-samples`
 const testTempOutputPath = `${basePath}/npx-tests`
 
-describe('gen-env-template file based e2e tests', () => {
+describe('gen-env-template file based npx tests', () => {
   beforeAll(() => {
     shell.exec(`mkdir ${testTempOutputPath}`)
   })
@@ -14,23 +14,21 @@ describe('gen-env-template file based e2e tests', () => {
     shell.exec(`rm -r ${testTempOutputPath}`)
   })
 
-  test('should generate template files as per samples', async () => {
-    const dirList = await promises.readdir(basePath, { withFileTypes: true })
+  const dirList = readdirSync(basePath, { withFileTypes: true })
 
-    const expectations = dirList
-      .filter((entry) => entry.isFile() && entry.name.includes('input'))
-      .map(async (fileEntry) => {
+  dirList
+    .filter((entry) => entry.isFile() && entry.name.includes('input'))
+    .forEach((fileEntry) => {
+      test(`npx file test case: '${fileEntry.name.replace('.input.env', '')}'`, () => {
         const inputFilePath = `${basePath}/${fileEntry.name}`
         const outputFilePath = `${testTempOutputPath}/${fileEntry.name.replace('input', 'output')}`
         const expectedOutputFilePath = `${basePath}/${fileEntry.name.replace('input', 'output')}`
 
         shell.exec(`npx -p file:/${process.env.GEN_ENV_TEMPLATE_BASE} ${packageName} ${inputFilePath} ${outputFilePath}`)
 
-        const expectedOutputString = await promises.readFile(expectedOutputFilePath, { encoding: 'utf-8' })
-        const actualOutputString = await promises.readFile(outputFilePath, { encoding: 'utf-8' })
+        const expectedOutputString = readFileSync(expectedOutputFilePath, { encoding: 'utf-8' })
+        const actualOutputString = readFileSync(outputFilePath, { encoding: 'utf-8' })
         expect(actualOutputString).toEqual(expectedOutputString)
       })
-
-    await Promise.all(expectations)
-  })
+    })
 })
