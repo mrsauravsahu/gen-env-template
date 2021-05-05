698a8c46c77b3488f2a37fb243a1a76fe6e34c9c
Saurav Sahu <mrsauravsahu@outlook.com>
2021-05-05 21:19:26 +0530

    fix: run through npx (#16)
    * fix: shebang to interpret cli.js with node
    
    * - run tests through npx
    - pass codebase dir as env to run npx tests
    
    * tofix: update testTimeout to 20000

--- __tests__/npx-tests.js
+++ __tests__/npx-tests.js
@@ -1,9 +1,9 @@
 const { promises } = require('fs')
 const shell = require('shelljs')
+const { name: packageName } = require('../package.json')
 
 const basePath = `${__dirname}/file-samples`
-const testTempOutputPath = `${basePath}/e2e`
-const bin = `node ${process.env.GEN_ENV_TEMPLATE_ABSOLUTE_PATH}`
+const testTempOutputPath = `${basePath}/npx-tests`
 
 describe('gen-env-template file based e2e tests', () => {
   beforeAll(() => {
@@ -24,7 +24,7 @@ describe('gen-env-template file based e2e tests', () => {
         const outputFilePath = `${testTempOutputPath}/${fileEntry.name.replace('input', 'output')}`
         const expectedOutputFilePath = `${basePath}/${fileEntry.name.replace('input', 'output')}`
 
-        shell.exec(`${bin} ${inputFilePath} ${outputFilePath}`)
+        shell.exec(`npx -p file:/${process.env.GEN_ENV_TEMPLATE_BASE} ${packageName} ${inputFilePath} ${outputFilePath}`)
 
         const expectedOutputString = await promises.readFile(expectedOutputFilePath, { encoding: 'utf-8' })
         const actualOutputString = await promises.readFile(outputFilePath, { encoding: 'utf-8' })
