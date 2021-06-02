f36c4c70b57b8b09448e8973019c563b55af5e91
Saurav Sahu <mrsauravsahu@outlook.com>
2021-06-03 00:28:56 +0530

    remove name flag

--- src/command.js
+++ src/command.js
@@ -41,7 +41,6 @@ GenEnvTemplateCommand.flags = {
   version: flags.version({ char: 'v' }),
   // add --help flag to show CLI version
   help: flags.help({ char: 'h' }),
-  name: flags.string({ char: 'n', description: 'name to print' }),
 }
 
 module.exports = GenEnvTemplateCommand
