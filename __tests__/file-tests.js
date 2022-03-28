c2c4328422a9ce56e0f316761431614b0a354f6a
Saurav Sahu <mrsauravsahu@outlook.com>
2022-03-29 02:37:04 +0530

    chore: use path.sep

--- __tests__/file-tests.js
+++ __tests__/file-tests.js
@@ -1,8 +1,8 @@
 const { readFileSync, readdirSync } = require('fs')
-const { normalize } = require('path')
+const { sep } = require('path')
 const genEnvTemplate = require('../src/gen-env-template')
 
-const basePath = `${__dirname}/file-samples`
+const basePath = `${__dirname}${sep}file-samples`
 
 describe('gen-env-template file tests', () => {
   const dirList = readdirSync(basePath, { withFileTypes: true })
@@ -11,9 +11,9 @@ describe('gen-env-template file tests', () => {
     .filter((entry) => entry.isFile() && entry.name.includes('input'))
     .forEach((fileEntry) => {
       test(`shell file test case: '${fileEntry.name.replace('.input.env', '')}'`, async () => {
-        const inputFilePath = `${basePath}/${fileEntry.name}`
-        const fileString = readFileSync(normalize(inputFilePath), { encoding: 'utf-8' })
-        const expectedOutput = readFileSync(normalize(inputFilePath).replace('input', 'output'), { encoding: 'utf-8' })
+        const inputFilePath = `${basePath}${sep}${fileEntry.name}`
+        const fileString = readFileSync(inputFilePath, { encoding: 'utf-8' })
+        const expectedOutput = readFileSync(inputFilePath.replace('input', 'output'), { encoding: 'utf-8' })
         const receivedOutput = genEnvTemplate(fileString)
         expect(receivedOutput).toEqual(expectedOutput)
       })
