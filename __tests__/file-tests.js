8a56f1fd56a97a33f32598f0729212e3398e95c3
Saurav Sahu <mrsauravsahu@outlook.com>
2022-03-29 02:32:49 +0530

    chore: use normalize path

--- __tests__/file-tests.js
+++ __tests__/file-tests.js
@@ -1,4 +1,5 @@
 const { readFileSync, readdirSync } = require('fs')
+const { normalize } = require('path')
 const genEnvTemplate = require('../src/gen-env-template')
 
 const basePath = `${__dirname}/file-samples`
@@ -11,8 +12,8 @@ describe('gen-env-template file tests', () => {
     .forEach((fileEntry) => {
       test(`shell file test case: '${fileEntry.name.replace('.input.env', '')}'`, async () => {
         const inputFilePath = `${basePath}/${fileEntry.name}`
-        const fileString = readFileSync(inputFilePath, { encoding: 'utf-8' })
-        const expectedOutput = readFileSync(inputFilePath.replace('input', 'output'), { encoding: 'utf-8' })
+        const fileString = readFileSync(normalize(inputFilePath), { encoding: 'utf-8' })
+        const expectedOutput = readFileSync(normalize(inputFilePath).replace('input', 'output'), { encoding: 'utf-8' })
         const receivedOutput = genEnvTemplate(fileString)
         expect(receivedOutput).toEqual(expectedOutput)
       })
